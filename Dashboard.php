ad202631b056b8ccf9f6a94c330db59016118429
AbdulRehman56 <211370196@gift.edu.pk>
2023-08-19 21:59:36 +0500

    up

--- Dashboard.php
+++ Dashboard.php
@@ -43,9 +43,29 @@
 
  <?php
  //sh_id	weight	category	issue_date	delievery_date	m_id	s_id	c_id
-  include ("navBar.php");
+  //include ("navBar.php");
   include ("connection.php");
 ?> 
+
+<nav>
+        <ul>
+          <li><a href="Dashboard.php">DashBoard</a></li>
+          <li><a href="AssignShipment.php">Assign</a></li>
+          <li><a href="#">Add</a>
+            <ul>
+              <li><a href="ShipmentDetail.php">Shippment</a></li>
+              <li><a href="Add_Courier.php">Courier</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Manage</a>
+              <ul>
+                  <li><a href="Manage_Shipment.php">Shippment</a></li>
+                  <li><a href="Manage_Courier.php">Courier</a></li>
+              </ul>
+          </li>
+        </ul>
+        <button><a href="">Logout</a></button>
+      </nav>
 
       <main> 
           <div class="container">
