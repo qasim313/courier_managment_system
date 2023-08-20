7692147dba8da384626a4e7776b9b68b1c3433db
hashir <hashirshafique44@gmail.com>
2023-08-20 21:42:34 +0500

    resolve issue of fetching ids

--- ShippementData.php
+++ ShippementData.php
@@ -12,7 +12,6 @@
     $s_name = $_REQUEST['s-name'];
     $s_address = $_REQUEST['s-address'];
     $s_phone = $_REQUEST['s-ph'];
-    $s_id = $_REQUEST['s-id'];
 
     $r_name = $_REQUEST['r-name'];
     $r_contact = $_REQUEST['r-contact'];
@@ -25,19 +24,27 @@
     $result = $stmt->FETCH_ASSOC();
     $m_id = $result['m_id'];
 
-    $sql1 = "insert into shipper(s_id,name,address,contact,m_id)
-            values('$s_id','$s_name','$s_address','$s_phone','$m_id')";
+    $sql1 = "insert into shipper(name,address,contact,m_id)
+            values('$s_name','$s_address','$s_phone','$m_id')";
 
     $connect->query($sql1);
 
-    $sql2 = "insert into shipment(weight,category,issue_date,delievery_date,m_id,s_id)
-            values('$sh_weight','$category','$p_date','$d_date','$m_id','$s_id')";
-   
-    $connect->query($sql2);
+
+    $sql6 = "select s_id from shipper order by s_id desc";
+    $result1 = $connect->query($sql6);
+
+    $row1= $result1->fetch_assoc();
+
+    $s_id = $row1['s_id'];
+
+    echo $s_id;
+
 
     
-   
-    
+    $sql2 = "insert into shipment(weight,category,issue_date,delievery_date,m_id,s_id)
+            values('$sh_weight','$category','$p_date','$d_date','$m_id','$s_id')";
+
+    $connect->query($sql2);
 
     $sql3 = "insert into customer(name,phone,address)
             values('$r_name','$r_contact','$r_address')";
@@ -51,20 +58,20 @@
         $amount = $amount + ($sh_weight - 5)*50;
     }//add 50RS per kg for weights more than 100
 
-    //we will add tax on the basis of location
+    //we will add tax on the basis of shippemnt category
 
     $tax = 0.0;
     if($category == "Document shipments"){
         $tax = 0.13;
     }
-    else if($category === "Package shipments"){
+    else if($category == "Package shipments"){
         $tax = 0.18;
     }
     else if($category == "Fragile shipments"){
         $tax = 0.23;
     }else if($category == "Express shipments"){
         $tax = 0.28;
-    }elseif ($category == "High Value shipments") {
+    }else if ($category == "High Value shipments") {
         $tax = 0.33;
     }
 
@@ -75,6 +82,16 @@
 
     $sql4 = "insert into payment(amount,tax,total,m_id)
             values('$amount','$taxAmount','$totalAmount','$m_id')";
+
+    $sql5 = "select p_id from payment order by issue_date desc";
+
+    $result2 = $connect->query($sql5);
+
+    $row2= $result2->fetch_assoc();
+
+    $p_id = $row2['p_id'];
+
+    echo "<br>".$p_id;
 
     $connect->query($sql4);
 
