50e021eaa834d48fa3d3d422fb4ac57c5630350f
Qasim Malik <qasimmalik808@gmail.com>
2023-08-21 17:23:44 +0500

    courier view

--- checkCredentials.php
+++ checkCredentials.php
@@ -1,28 +1,33 @@
 <?php
-
+    session_start();
     include ('connection.php');
 
 
     $id = $_REQUEST['id'];
     $pass = $_REQUEST['password'];
-    
     $sql = "select name from courier where password = $pass and c_id = '$id'";
     $result = mysqli_query($connect, $sql);
     if($result && $result->num_rows == 1){
         $row = mysqli_fetch_assoc($result);
-        echo "$row[name]
-        <script>
-            location.href=\"dashboard.php\";
-        </script>";
+        $_SESSION['authenticated'] = true;
+        header("Location: courier_view.php?value=$id"); 
+        exit();
+        // echo "
+        // <script>
+        //     location.href=\"courier_view.php?value=\"+$id;
+        // </script>";
     }else{
         $sql1 = "select name from manager where password = '$pass' and m_id = '$id'";
         $result1 = mysqli_query($connect, $sql1);
         if($result1 && $result1->num_rows == 1){
             $row = mysqli_fetch_assoc($result1);
-            echo "$row[name]
-            <script>
-                location.href=\"dashboard.php\";
-            </script>";
+            $_SESSION['authenticated'] = true;
+            header("Location: dashboard.php"); 
+            exit();
+            // echo "$row[name]
+            // <script>
+            //     location.href=\"dashboard.php\";
+            // </script>";
         }else{
             echo "<script>
             alert(\"Incorrect Password\");
--- update_courier.php
+++ update_courier.php
@@ -1,80 +1,16 @@
 
 <?php
-    include('connection.php');
 
-    $c_id = $_GET['c_id'];
-    $c_name = $_GET['name'];
-    $status = $_GET['status'];
-    $pass = $_GET['PASSWORD'];
-    $c_contact = $_GET['contact'];
-    $c_location = $_GET['address'];
+    include("connection.php");
+
+    if (isset($_POST['sh_id']) && isset($_POST['new_status'])) {
+        $sh_id = $_POST['sh_id'];
+        $new_status = $_POST['new_status'];
+
+        $sql = "update shipment set status = '$new_status' where sh_id = $sh_id";
+        $result=$connect->query($sql);
+        if($result)echo"status updated!";
+        else echo "failed to update status";
+    }
+    $connect->close();
 ?>
-<!DOCTYPE html>
-<html >
-<head>
-<link rel="stylesheet" href="./Css/CourierFormStyling.css" />
-
-    <title>Upadte_courier</title>
-</head>
-<body>
-
-    <?php
-    
-        
-    ?>
-
-
-
-<div class="container">
-      <form method="post" class="item1">
-        <h3 class="item1-1">Upadte Detail</h3>
-
-        <label for="id">Courier id:</label>
-        <input type="text" name="id" id="id" class="item1-2" value ="<?php echo $c_id; ?>"  readonly/>
-        <label for="name">Courier Name:</label>
-        <input type="text" name="name" id="name" class="item1-2" value ="<?php echo $c_name; ?>" readonly />
-        <label for="phone">Contact no:</label>
-        <input type="tel" name="ph" id="ph" class="item1-2"  value ="<?php echo $c_contact; ?>"/>
-        <label for="location">Address:</label>
-        <input type="text" name="location" id="" value ="<?php echo $c_contact; ?>">
-        <label for="password">password:</label>
-        <input type="text" name="password" id="" value ="<?php echo $pass; ?>">
-
-        <label for="">status:</label>
-        <select name="status" id="status">
-          <option value="active">active</option>
-          <option value="inactive">inactive</option>
-        </select>
-
-        <br />
-        <input type="submit" value="Submit" id="btn" name="enter" />
-      </form>
-    </div>
-
-    <?php
-       
-    
-        if (isset($_POST['enter'])) {
-            $c_id = $_POST['id'];
-            $c_name = $_POST['name'];
-            $c_location = $_POST['location'];
-            $c_contact = $_POST['ph'];
-            $pass = $_POST['password'];
-            $status = $_POST['status'];
-            $sql ="UPDATE `courier` SET `status`='$status',`PASSWORD`='$pass',`address`='$c_location',`contact`='$c_contact' WHERE c_id = $c_id";
-            $result =$connect->query($sql);
-            if ($result) {
-                echo "yes";
-                echo "<script>location.href='courier.php'</script>";
-            } else {
-                echo "no";
-                echo "<script>location.href='courier.php'</script>";
-            }
-        }
-
-
-    ?>
-
-    
-</body>
-</html>
