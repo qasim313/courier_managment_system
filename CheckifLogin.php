0cdb5f17319ce05130217c4506f07f32200b1267
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2023-08-23 13:11:04 -0700

    sessions updated

--- CheckifLogin.php
+++ CheckifLogin.php
@@ -13,7 +13,17 @@
     // else{
 
         session_start();
-  
+        if(isset($_SESSION['courier']) && $_SESSION['courier']){
+                    //exit and redirect to login.html if user is trying to open anyother file than "courier_view.php"
+            if(basename($_SERVER['PHP_SELF']) != "courier_view.php"){
+                if(basename($_SERVER['PHP_SELF']) != "updateShipmentStatus.php"){
+                    header("Location: login.html"); 
+                    exit();
+                }
+                
+            }
+
+        }
         if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
             header("Location: login.html"); // Redirect to the login page
             exit();
--- checkCredentials.php
+++ checkCredentials.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
     include ('connection.php');
-    echo "login";
+    // echo "login";
 
     $id = $_REQUEST['id'];
     $pass = $_REQUEST['password'];
-    $sql = "select name from courier where password = $pass and c_id = '$id'";
+    $sql = "select name from courier where password = '$pass' and c_id = '$id'";
     $result = mysqli_query($connect, $sql);
 
 
@@ -13,6 +13,7 @@
         
         $row = mysqli_fetch_assoc($result);
         $_SESSION['authenticated'] = true;
+        $_SESSION['courier'] = true;
         header("Location: courier_view.php?value=$id"); 
         exit();
         // echo "
@@ -25,6 +26,7 @@
         if($result1 && $result1->num_rows == 1){
             $row = mysqli_fetch_assoc($result1);
             $_SESSION['authenticated'] = true;
+            $_SESSION['courier'] = false;
             header("Location: dashboard.php"); 
             exit();
             // echo "$row[name]
